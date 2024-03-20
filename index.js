db841631f6c137183a8b3070a185795586066c6d
eric <ericmaina11455@gmail.com>
2024-03-20 22:55:14 +0300

    adding-firstclass-functions

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+
+function returnsANamedFunction() {
+    function namedFunction() {
+        
+        console.log("This is a named function.");
+    }
+    
+    return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    
+    return function() {
+        
+        console.log("This is an anonymous function.");
+    };
+}
+
+receivesAFunction(function() {
+    console.log("Callback function called.");
+});
+
+const namedFunc = returnsANamedFunction();
+
+namedFunc();
+
+
+returnsAnAnonymousFunction()();
